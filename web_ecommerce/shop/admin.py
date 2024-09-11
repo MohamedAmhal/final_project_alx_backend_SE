60db863c18c7846c6408c2a8e215ce64ac9baeaa
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-12 00:11:45 +0100

    adding the Product and Category models to the panal admin :

--- web_ecommerce/shop/admin.py
+++ web_ecommerce/shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# .
+# register the model product.
+admin.site.register(Product)
+
+# register the model Category.
+admin.site.register(Category)
+
