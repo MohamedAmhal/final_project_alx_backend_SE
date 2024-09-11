2a9fa0a9f3f30bf788abf028fd5aa13bac88acda
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-12 00:08:13 +0100

    adding the first to models code in models.py:

--- web_ecommerce/shop/models.py
+++ web_ecommerce/shop/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models import ForeignKey
 
-# Create your models here.
+# class category
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    added_date = models.DateTimeField(auto_now=True)
+
+    #creation du premier vue
+    class Meta:
+        ord = ['-added_date']
+
+
+# class product
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    category = ForeignKey(Category, related_name='categorie', on_delete=models.CASCADE)
+    image = models.CharField(max_length=5000)
+    added_date = models.DateTimeField(auto_now=True)
+
+    #creation du premier vue
+    class Meta:
+        ord = ['-added_date']
