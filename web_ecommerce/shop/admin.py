4c77a468f091648edc157abf76410c2e9aaf309c
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-12 00:43:31 +0100

    adding some features in the tables for the panal admine:

--- web_ecommerce/shop/admin.py
+++ web_ecommerce/shop/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Category, Product
 
+
+# displaying the features in panal tables (for category table) :
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name', 'added_date')
+
+
+# displaying the features in panal tables (for product table) :
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category', 'added_date')
+
+
 # register the model product.
-admin.site.register(Product)
+admin.site.register(Product, AdminProduct)
 
 # register the model Category.
-admin.site.register(Category)
-
+admin.site.register(Category, AdminCategory)
