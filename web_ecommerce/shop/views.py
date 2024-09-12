c8dcd439ab9a1dd161d4d84da12fc42329a26b76
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-12 16:43:31 +0100

    fix the number of product that display it in the home page :

--- web_ecommerce/shop/views.py
+++ web_ecommerce/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create the first view
 def index(request):
@@ -11,5 +12,7 @@ def index(request):
     if item_name != '' and item_name is not None:
         #je vais un velo ==> velo display  so this is the objectif of itle__icontains
         product_object = Product.objects.filter(title__icontains = item_name)
-
+    paginator = Paginator(product_object, 8)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
     return render(request, 'shop/index.html', {'product_object' : product_object})
