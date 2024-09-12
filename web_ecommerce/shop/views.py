e771919f6c2a8438f103cc0532f69863c48aab27
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-12 16:09:02 +0100

    fix the search function in backend and also in frontend:

--- web_ecommerce/shop/views.py
+++ web_ecommerce/shop/views.py
@@ -6,4 +6,10 @@ def index(request):
     # display images in frontend
     # 1. select all prduct that exist in the database
     product_object = Product.objects.all()
+    # 2. search items
+    item_name = request.GET.get('item-name')
+    if item_name != '' and item_name is not None:
+        #je vais un velo ==> velo display  so this is the objectif of itle__icontains
+        product_object = Product.objects.filter(title__icontains = item_name)
+
     return render(request, 'shop/index.html', {'product_object' : product_object})
