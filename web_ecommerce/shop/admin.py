1173e7c257b4ba61f577336255c4c7b40c8769e4
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-13 21:51:52 +0100

    adding some changes in the admin page

--- web_ecommerce/shop/admin.py
+++ web_ecommerce/shop/admin.py
@@ -2,18 +2,28 @@ from django.contrib import admin
 from .models import Category, Product, Commande
 
 
+#parameters of the admin page 
+admin.site.site_header = "E-commerce Center"
+admin.site.site_title = "Mohammed Shop"
+admin.site.index_title = "Manadger"
+
 # displaying the features in panal tables (for category table) :
 class AdminCategory(admin.ModelAdmin):
     list_display = ('name', 'added_date')
+    search_fields = ('name', )
 
 
 # displaying the features in panal tables (for product table) :
 class AdminProduct(admin.ModelAdmin):
     list_display = ('title', 'price', 'category', 'added_date')
+    search_fields = ('title', )
+    list_editable = ('price', 'category',)
 
 # displaying the features in panal tables (for Commande table) :
 class AdminCommande(admin.ModelAdmin):
     list_display = ('items', 'name', 'email', 'address', 'city', 'total', 'command_date')
+    search_fields = ('city', )
+    list_editable = ('name', 'email', 'address', 'city', 'total',)
 
 
 
