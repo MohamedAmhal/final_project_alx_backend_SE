77965b523da45c772c469c828bcc24afb9a7952a
MohamedAmhal <mohammedamhal349@gmail.com>
2024-09-13 16:55:24 +0100

    adding the command model in the admin page

--- web_ecommerce/shop/admin.py
+++ web_ecommerce/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Commande
 
 
 # displaying the features in panal tables (for category table) :
@@ -11,9 +11,17 @@ class AdminCategory(admin.ModelAdmin):
 class AdminProduct(admin.ModelAdmin):
     list_display = ('title', 'price', 'category', 'added_date')
 
+# displaying the features in panal tables (for Commande table) :
+class AdminCommande(admin.ModelAdmin):
+    list_display = ('name', 'email', 'address', 'city', 'zip', 'command_date')
+
+
 
 # register the model product.
 admin.site.register(Product, AdminProduct)
 
 # register the model Category.
 admin.site.register(Category, AdminCategory)
+
+# register the model Commande.
+admin.site.register(Commande, AdminCommande)
